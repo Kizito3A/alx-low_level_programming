5f87bc0dbcb1bf3d07629bf0e2d4b5e66ca06d59
kizito <kizitoamandi90@gmail.com>
2022-06-04 02:36:48 -0700

    Task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *My hello,print test
+ *Return:Return 1 after the program execution
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
