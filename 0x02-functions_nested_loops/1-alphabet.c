0fb5a62adfd917f2e370ffa1bb3cbec0e86cf1c5
kizito <kizitoamandi90@gmail.com>
2022-06-07 14:29:41 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+void print_alphabet(void)
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++);
+
+{
+_putchar(alphabet);
+}
+
+_putchar('\n');
+
+}
+
