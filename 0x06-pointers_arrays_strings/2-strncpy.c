ca06405b16ef006cb3c8eb7dbc1828e2ada2de7f
kizito <kizitoamandi90@gmail.com>
2022-06-16 06:58:43 -0700

    task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- *_strncpy -copies a string
- *@dest: A pointer a character that will be changed
- *@src: A pointer to a charaacter that will be changed
- *@n: value 
+ *_strncpy - copies a string
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will be changed
+ *@n: value
  *Return: dest
- */
+ */ 
 
 char *_strncpy(char *dest, char *src, int n)
 {
