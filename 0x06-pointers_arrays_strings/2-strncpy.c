166d8b5d2a65b59d44713d6e3e75262f57acbf37
kizito <kizitoamandi90@gmail.com>
2022-06-16 06:49:00 -0700

    task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,27 +3,27 @@
  * _strncpy -copies a string
  * @dest: A pointer a character that will be changed
  * @src: A pointer to a charaacter that will be changed
- * @n: value
- * Return: the pointer to dest. 
+ * @n: value * Return: the pointer to dest. 
+ * Return: dest
  */
-char *_strncpy(char *dest, char *src, int n)
 
+char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 
 i = 0;
 while (i < n && src[i] != '\0')
 {
+
 dest[i] = src[i];
 i++;
 }
 
 while (i < n)
 {
+
 dest[i] = '\0';
 i++;
 }
-
 return (dest);
-
 }
