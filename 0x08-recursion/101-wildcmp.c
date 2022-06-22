98f8b2c0f6cfd0ae40a0e21a42eea1a64a6c6de6
kizito <kizitoamandi90@gmail.com>
2022-06-22 07:48:57 -0700

    task9

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,12 +6,12 @@
  * @s2: pointer to string params
  * Return: 0
  */
+
 int wildcmp(char *s1, char *s2)
 {
 if (*s1 == '\0')
 {
 if (*s2 != '\0' && *s2 == '*')
-
 {
 return (wildcmp(s1, s2 + 1));
 }
@@ -23,9 +23,8 @@ if (*s2 == '*')
 return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 }
 else if (*s1 == *s2)
-{
+}
 return (wildcmp(s1 + 1, s2 + 1));
 }
 return (0);
 }
-	
